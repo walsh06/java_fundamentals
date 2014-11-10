81664305bf4cf7d67e81839b6cf9f1f7732d8a8f
walsh06 <liammail06@gmail.com>
2014-11-10 21:47:19 +0000

    Added comments

--- Array3.java
+++ Array3.java
@@ -2,16 +2,24 @@ public class Array3
 {
 	public static void main(String[] args)
 	{
+		//declare variables
 		int[] lottoNumbers = new int[6];
 		int numbersCount = 0, currentNum;
 		boolean numFound = false;
+
+		//keep looping until 6 numbers have been added
 		while(numbersCount < 6)
-		{
+		{	
+			//reset boolean variable to false
 			numFound = false;
+
+			//generate random number
 			currentNum = (int)(Math.random() * 48) + 1;
 
+			//loop through all numbers
 			for(int i =0; i < lottoNumbers.length; i++)
 			{
+				//if the random number is already in the array
 				if(lottoNumbers[i] == currentNum)
 				{
 					numFound = true;
@@ -19,6 +27,8 @@ public class Array3
 				}
 			}
 
+			//if the number was not already there
+			//add it in and increment the count
 			if(!numFound)
 			{
 				lottoNumbers[numbersCount] = currentNum;
@@ -26,6 +36,7 @@ public class Array3
 			}
 		}
 
+		//Print out result
 		System.out.println("The winning numbers were: ");
 		for(int i =0; i < lottoNumbers.length; i++)
 		{
